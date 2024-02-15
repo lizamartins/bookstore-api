e52fa2bc88924952ae12e7155b032a990d43a2da
lizamartins <lufernandes.mt@gmail.com>
2024-02-15 18:23:01 -0300

    Adiciona swagger

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -2,6 +2,7 @@ package com.example.bookstore.controller;
 
 import com.example.bookstore.exception.BookNotFoundException;
 import com.example.bookstore.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,28 +25,33 @@ public class BookController {
 	@Autowired
 	BookService bookService;
 
+	@Operation(summary= "Add a new book to the store", tags= "Books")
 	@PostMapping
 	public ResponseEntity<BookModel> saveBook(@RequestBody BookRecordDto bookRecordDto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(bookService.saveBook(bookRecordDto));
 	}
 
-	@GetMapping
+	@Operation(summary= "Get all books from the store", tags="Books")
+	@GetMapping("/all")
 	public ResponseEntity<Page<BookModel>> getAllBooks(Pageable pageable) {
 		return ResponseEntity.status(HttpStatus.OK).body(bookService.getAllBooks(pageable));
 	}
 
+	@Operation(summary= "Find book by ID", tags="Books")
 	@GetMapping("/{id}")
 	public ResponseEntity<Object> getOneBook(@PathVariable(value="id") UUID id) {
 		var response = bookService.getOneBook(id);
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
+	@Operation(summary= "Update an existing book by ID", tags="Books")
 	@PutMapping("/{id}")
 	public ResponseEntity<Object> updateBook(@PathVariable(value="id") UUID id, @RequestBody @Valid BookRecordDto bookRecordDto) {
 		var response = bookService.updateBook(id, bookRecordDto);
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
+	@Operation(summary= "Delete a book from the store", tags="Books")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Object> deleteBook(@PathVariable(value="id") UUID id) {
 		return ResponseEntity.status(HttpStatus.OK).body(bookService.deleteBook(id));
